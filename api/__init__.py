8e5603b5b583223cbc68245410d24f9dd99b5d39
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-10-07 21:08:41 +0300

    add environ

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
-import os
-SECRET_KEY = os.urandom(32)
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
 
 
 port = int(os.environ.get("PORT", 5000))
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config.from_object(Config)
+# app.config['SECRET_KEY'] = SECRET_KEY
 
 from api import routes
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_APP = 'api/app.py'
+    SECRET_KEY = os.urandom(32)
+    PORT = 5000
